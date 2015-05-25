01a994504134588f32a6ceea58e2fbedf3e3ca8d
Simon Mezzomo <simlt@users.noreply.github.com>
2015-05-25 16:15:25 +0200

    Implemented initial database support

---
+++ sql/database.sql
@@ -0,0 +1,78 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.12
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost:8889
+-- Generation Time: Mag 18, 2015 alle 18:34
+-- Versione del server: 5.5.34-log
+-- PHP Version: 5.5.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+--
+-- Database: `win`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `restroom`
+--
+
+DROP TABLE IF EXISTS `restroom`;
+CREATE TABLE `restroom` (
+  `id` int(11) NOT NULL,
+  `people_count` int(11) NOT NULL DEFAULT 0,
+  `wc_count` int(11) NOT NULL,
+  `gender` char(2), NOT NULL,
+  `status` int(11) DEFAULT NULL,
+  `wc_closed_count` int(11) DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `distance`
+--
+
+DROP TABLE IF EXISTS `distance`;
+CREATE TABLE `distance` (
+  `restroom` int(11) NOT NULL,
+  `place` int(11) NOT NULL,
+  `priority` int(11) NOT NULL,
+  PRIMARY KEY (`restroom`,`place`),
+  KEY `place` (`place`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `place`
+--
+
+DROP TABLE IF EXISTS `place`;
+CREATE TABLE `place` (
+  `id` int(11) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `type` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `restroom`
+--
+ALTER TABLE `restroom`
+  ADD CONSTRAINT `restroom_ibfk_1` FOREIGN KEY (`id`) REFERENCES `place` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Limiti per la tabella `distance`
+--
+ALTER TABLE `distance`
+  ADD CONSTRAINT `distance_ibfk_2` FOREIGN KEY (`place`) REFERENCES `place` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `distance_ibfk_1` FOREIGN KEY (`restroom`) REFERENCES `restroom` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ src/database.py
@@ -0,0 +1,113 @@
+'''
+Created on 21/mag/2015
+
+@author: Simon Mezzomo
+'''
+
+import ConfigParser
+import mysql.connector
+
+configParser = ConfigParser.RawConfigParser()
+
+
+# Prepared statements
+stmt_placeID = "SELECT `id`, `name`, `type` FROM `place` WHERE `id` = %s"
+stmt_placeName = "SELECT `id`, `name`, `type` FROM `place` WHERE `name` = %s"
+stmt_placeType = "SELECT `id`, `name`, `type` FROM `place` WHERE `type` = %s"
+
+stmt_restroomID = "SELECT `id`, `people_count`, `wc_count`, `status`, `wc_closed_count` FROM place WHERE `id` = %s"
+
+#stmt_distanceRestroomFromPlace = "SELECT `priority` FROM `distance` WHERE `restroom` = %s AND `place` = %s"
+stmt_distanceRestroomList = "SELECT `restroom`, `priority` FROM `distance` WHERE `place` = %s"
+
+
+class Database(object):
+
+    def __init__(self, cfgFilename):
+        """Create a DB object.
+        :cfgFilename filename for database connection configuration
+        """
+        configParser.read(cfgFilename)
+        user = configParser.get('Database', 'user')
+        password = configParser.get('Database', 'password')
+        host = configParser.get('Database', 'host')
+        database = configParser.get('Database', 'database')
+        self.conn = mysql.connector.connect(user=user, password=password, host=host, database=database)
+    
+    def close(self):
+        self.conn.close()
+
+    """
+    Place queries
+    """
+    def getPlaceInfobyID(self, placeID):
+        if isinstance(placeID, int) == False:
+            raise TypeError("argument must be of int type")
+
+        cur = self.conn.cursor()
+        cur.execute(stmt_placeID, (placeID,))
+        r = cur.fetchone()
+        if r == None:
+            print 'Warning: getPlaceInfobyID no entry found for place ID "' + str(placeID) + '"' 
+        cur.close()
+        
+        return {'id': r[0], 'name': r[1], 'type': r[2]}
+    
+    def getPlaceInfobyName(self, name):
+        cur = self.conn.cursor()
+        cur.execute(stmt_placeName, (name,))
+        r = cur.fetchone()
+        if r == None:
+            print 'Warning: getPlaceInfobyName no entry found for name "' + name + '"'
+        cur.close()
+        
+        return {'id': r[0], 'name': r[1], 'type': r[2]}
+    
+    def getPlaceInfobyType(self, typestr):
+        cur = self.conn.cursor()
+        cur.execute(stmt_placeType, (typestr,))
+        r = cur.fetchall()
+        if len(r) == 0:
+            print 'Warning: getPlaceInfobyType no entry found for type "' + typestr + '"'
+        cur.close()
+        
+        #places = []
+        # TODO for row in r:
+        return r # {'id': r[0], 'name': r[1], 'type': r[2]}
+
+    """
+    Restrooom queries
+    """
+    def getRestroomInfobyID(self, restroomID):
+        if isinstance(restroomID, int) == False:
+            raise TypeError("argument must be of int type")
+
+        cur = self.conn.cursor()
+        cur.execute(stmt_restroomID, (restroomID,))
+        r = cur.fetchone()
+        if r == None:
+            print 'Warning: getRestroomInfobyID no entry found for restroom ID "' + str(restroomID) + '"'
+        cur.close()
+        
+        return {'id': r[0], 'peopleCount': r[1], 'wcCount': r[2], 'status': r[3], 'wcClosedCount': r[4]}
+
+    """
+    Distance queries
+    """
+    def getPriorityListFromPlace(self, placeID):
+        if isinstance(placeID, int) == False:
+            raise TypeError("argument must be of int type")
+
+        cur = self.conn.cursor()
+        cur.execute(stmt_restroomID, (placeID,))
+        r = cur.fetchall()
+        if len(r) == 0:
+            print 'Warning: getPriorityListFromPlace no entry found for place ID "' + str(placeID) + '"' 
+        cur.close()
+        
+        return r
+    
+""" Instance of Database class to be used.
+Avoid creating other instances of the class, otherwise each class will create a new connection!
+"""
+DB = Database('db.cfg')
