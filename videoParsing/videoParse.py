50fd04cced6a9129be3b1a530a7837e10210e0a6
ThomasVitale <ThomasVitale@users.noreply.github.com>
2015-05-25 16:51:27 +0200

    5 seconds loop added

--- videoParsing/videoParse.py
+++ videoParsing/videoParse.py
@@ -2,20 +2,24 @@
 
 from bs4 import BeautifulSoup
 #from database import DB
+import time
 
 address = "http://IP:30003/gateflow.cgi?action=total&id=ID&direction=DIRECTION&begin=BEGIN&end=END&fromReset=value"
 
-markup = BeautifulSoup(open(address), "xml")
+while(True):
+    markup = BeautifulSoup(open(address), "xml")
 
-#addressTest = '<total><item total="34" direction="IN" id="0" /><item total="117" direction="OUT" id="0" /></total>'
-#file = BeautifulSoup(markupTest), "xml")
+    #addressTest = '<total><item total="34" direction="IN" id="0" /><item total="117" direction="OUT" id="0" /></total>'
+    #file = BeautifulSoup(markupTest), "xml")
 
-items = markup.find_all('item')
+    items = markup.find_all('item')
 
-for item in items:
-    if item['direction'] == "IN":
-        inCount = item['total']
-        print "IN %s" %(inCount)
-    else:
-        outCount = item['total']
-        print "OUT %s" %(outCount)
+    for item in items:
+        if item['direction'] == "IN":
+            inCount = item['total']
+            print "IN %s" %(inCount)
+        else:
+            outCount = item['total']
+            print "OUT %s" %(outCount)
+            
+    time.sleep(5)
