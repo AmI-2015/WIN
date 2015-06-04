d7d50b6190f882bbc5da9f75f7896f351c2c96f0
ThomasVitale <ThomasVitale@users.noreply.github.com>
2015-06-04 16:45:05 +0200

    edit jquery for form

--- src/server/static/win.js
+++ src/server/static/win.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	});
 	$("#typeofroom").change(function() {
 		$.getJSON("/placeType/"+ $("#typeofroom").val(), function(result) {
-			var select = $("#selectroom")
+			var select = $("#selectroom");
 			select.empty();
 			$.each(result.rooms, function(index, value) {
 				select.append($("<option />").val(value.id).text(value.name));
@@ -16,4 +16,13 @@ $(document).ready(function() {
 	$("#find").click(function() {
 		window.location = "/place/" + $("#selectroom").val();
 	});
+    
+    $("#typeofroom").change(function() {
+        $("#selectRoomForm").css("display", "block");
+    });
+    
+    $("#selectroom").change(function() {
+        $("#findForm").css("display", "block");
+    });
+    
 });
