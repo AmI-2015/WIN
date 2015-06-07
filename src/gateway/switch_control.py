66f633611afac50a62b3c0c416c1a8ab1f4626fc
Luca <vac93luca@gmail.com>
2015-06-07 09:38:20 +0200

    funzione switch lato gateway

--- src/gateway/switch_control.py
+++ src/gateway/switch_control.py
@@ -6,17 +6,27 @@ Created on 28/mag/2015
 
 import RPi.GPIO as GPIO
 import time
-from database import DB
+import requests
 
 def switchcontrol():
 	GPIO.setmode(GPIO.BOARD)
-    while (True):
-        GPIO.output(15,GPIO.HIGH)
-        time.sleep(0.1)
-        if(GPIO.input(13)==GPIO.LOW):
-            #lo stato basso, cioè uscita in cortocircuito con l'ingresso, indica bagno chiuso (1)
-            updateRestroomStatus(ID, 1) 
-        else
-            updateRestroomStatus(ID, 0)
-        GPIO.output(15,GPIO.LOW)
-        time.sleep(4.9)
+	Id=28
+	serverUrl='http://localhost:5000/updatestatus/'+Id
+	last=GPIO.LOW
+	flag=False
+	while (True):
+		GPIO.output(15,GPIO.HIGH)
+		time.sleep(0.1)
+		Input=GPIO.input(13)
+		if (last != Input and flag):
+			if(Input==GPIO.LOW):
+				#lo stato basso, cioe uscita in cortocircuito con l'ingresso, indica bagno chiuso (1)
+				data={ Id : 1}
+				requests.post(serverUrl, data)
+			else:
+				data={ Id : 0}
+				requests.post(serverUrl, data)
+		last=GPIO.input(13)
+		flag=True
+		GPIO.output(15,GPIO.LOW)
+		time.sleep(2.3)
