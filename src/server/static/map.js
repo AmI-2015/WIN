9178a66514aff0d174d776262f96be8f78db086c
Simon Mezzomo <simlt@users.noreply.github.com>
2015-06-18 20:47:32 +0200

    Convert spaces to tabs in js files

--- src/server/static/map.js
+++ src/server/static/map.js
@@ -1,74 +1,74 @@
-    function initialize() {
-            var nearest;
-            var zoom;
-            if (typeof indexPage === 'undefined')
-            {
-                //set the center of the map to the middle of Politecnico
-                nearest = new google.maps.LatLng(restrooms[0].lat, restrooms[0].long);
-                zoom = 18;
-            }
-            else // if (indexPage == true)
-            {
-                // coordinate of nearest restroom  **selected by user**
-                nearest = new google.maps.LatLng(45.06335, 7.660734);
-                zoom = 17;
-            }
+function initialize() {
+	var nearest;
+	var zoom;
+	if (typeof indexPage === 'undefined')
+	{
+		//set the center of the map to the middle of Politecnico
+		nearest = new google.maps.LatLng(restrooms[0].lat, restrooms[0].long);
+		zoom = 18;
+	}
+	else // if (indexPage == true)
+	{
+		// coordinate of nearest restroom  **selected by user**
+		nearest = new google.maps.LatLng(45.06335, 7.660734);
+		zoom = 17;
+	}
 
-            //set the center of the map to the nearest restroom
-            var mapOptions = {
-                zoom: zoom,
-                center: nearest
-            };
+	//set the center of the map to the nearest restroom
+	var mapOptions = {
+			zoom: zoom,
+			center: nearest
+	};
 
-            var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-            var marker =[];
-	        var infowindow =[];
+	var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+	var marker =[];
+	var infowindow =[];
 
-	        function markerCallback(marker,content,infowindow) {
-                return function() {
-                    infowindow.setContent(content);
-                    infowindow.open(map,marker);
-                };
-            }
+	function markerCallback(marker,content,infowindow) {
+		return function() {
+			infowindow.setContent(content);
+			infowindow.open(map,marker);
+		};
+	}
 
-            for (i = 0; i < restrooms.length; i++) {
-                //position
-                tempPosition = new google.maps.LatLng(restrooms[i].lat, restrooms[i].long);
-                //set the color of markers
-                if (restrooms[i].wc_available > restrooms[i].people_count && restrooms[i].status === 0)
-                    color = 'g';
-                else if (restrooms[i].wc_available == restrooms[i].people_count)
-                    color = 'y';
-                else
-                    color = 'r';
-                var image = '/static/images/' + restrooms[i].gender.toLowerCase() + color + ".png";
+	for (i = 0; i < restrooms.length; i++) {
+		//position
+		tempPosition = new google.maps.LatLng(restrooms[i].lat, restrooms[i].long);
+		//set the color of markers
+		if (restrooms[i].wc_available > restrooms[i].people_count && restrooms[i].status === 0)
+			color = 'g';
+		else if (restrooms[i].wc_available == restrooms[i].people_count)
+			color = 'y';
+		else
+			color = 'r';
+		var image = '/static/images/' + restrooms[i].gender.toLowerCase() + color + ".png";
 
-                //create marker
-                marker = new google.maps.Marker({
-                    position: tempPosition,
-                    map: map,
-                    title: restrooms[i].name,
-                    icon: image
-                });
+		//create marker
+		marker = new google.maps.Marker({
+			position: tempPosition,
+			map: map,
+			title: restrooms[i].name,
+			icon: image
+		});
 
-                //create content window
-                var contentString = restrooms[i].name + "<br>Toilets: " + (restrooms[i].wc_count - restrooms[i].wc_closed_count) +
-                    "<br>People: " + restrooms[i].people_count + "<br>Status: " + restrooms[i].status_str;
-                infowindow = new google.maps.InfoWindow();
+		//create content window
+		var contentString = restrooms[i].name + "<br>Toilets: " + (restrooms[i].wc_count - restrooms[i].wc_closed_count) +
+		"<br>People: " + restrooms[i].people_count + "<br>Status: " + restrooms[i].status_str;
+		infowindow = new google.maps.InfoWindow();
 
-                //create event in order to show info window
-                google.maps.event.addListener(marker,'click', markerCallback(marker,contentString,infowindow));
+		//create event in order to show info window
+		google.maps.event.addListener(marker,'click', markerCallback(marker,contentString,infowindow));
 
 
-                // add hallway overlay
-                var imageBounds = new google.maps.LatLngBounds(
-                        new google.maps.LatLng(45.060240762168780,7.654595375061035),
-                        new google.maps.LatLng(45.066787985689180,7.664896811256426));
-                var overlayOptions  = {clickable:false};
-				var overlay = new google.maps.GroundOverlay(
+		// add hallway overlay
+		var imageBounds = new google.maps.LatLngBounds(
+				new google.maps.LatLng(45.060240762168780,7.654595375061035),
+				new google.maps.LatLng(45.066787985689180,7.664896811256426));
+		var overlayOptions  = {clickable:false};
+		var overlay = new google.maps.GroundOverlay(
 				"http://www.polito.it/ateneo/sedi/images/sede_overlay.png", imageBounds, overlayOptions);
-				overlay.setMap(map);
-            }
+		overlay.setMap(map);
+	}
 
-        }
-        google.maps.event.addDomListener(window, 'load', initialize);
+}
+google.maps.event.addDomListener(window, 'load', initialize);
--- src/server/static/win.js
+++ src/server/static/win.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
 	});
 
 	$("#typeofroom").change(function() {
-        $("#selectRoomForm").css("display", "block");
+		$("#selectRoomForm").css("display", "block");
 		$.getJSON("/placeType/"+ $("#typeofroom").val(), function(result) {
 			var select = $("#selectroom");
 			select.empty();
@@ -21,19 +21,18 @@ $(document).ready(function() {
 			window.location = "/place/" + $("#selectroom").val() + "/" + $("input[name='gender']:checked").val() + "/";
 		}
 	});
-    
-    $("#selectroom").change(function() {
-        $("#findForm").css("display", "block");
-    });
-    
-    /* add class 'active' to nav item */
-    if (location.pathname.substring(1) == "index") {
-        $(".navbar-nav li:nth-child(1)").addClass("active");
-    } else if (location.pathname.substring(1) == "about") {
-        $(".navbar-nav li:nth-child(2)").addClass("active");
-    } else if (location.pathname.substring(1) == "contact") {
-        $(".navbar-nav li:nth-child(3)").addClass("active");
-    }
+
+	$("#selectroom").change(function() {
+		$("#findForm").css("display", "block");
+	});
+
+	/* add class 'active' to nav item */
+	if (location.pathname.substring(1) == "index") {
+		$(".navbar-nav li:nth-child(1)").addClass("active");
+	} else if (location.pathname.substring(1) == "about") {
+		$(".navbar-nav li:nth-child(2)").addClass("active");
+	} else if (location.pathname.substring(1) == "contact") {
+		$(".navbar-nav li:nth-child(3)").addClass("active");
+	}
 
 });
-
