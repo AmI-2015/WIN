1a0132a34e78d4321000ac80a53b2c6fdda70649
ThomasVitale <ThomasVitale@users.noreply.github.com>
2015-06-19 23:02:42 +0200

    add main def

--- src/server/videoParse.py
+++ src/server/videoParse.py
@@ -4,25 +4,29 @@ from bs4 import BeautifulSoup
 from database import DB
 import time
 
-address = "http://IP:30003/gateflow.cgi?action=total&id=ID&direction=DIRECTION&begin=BEGIN&end=END&fromReset=value"
+address = "http://IP:30003/gateflow.cgi?action=total&id=6&direction=DIRECTION&begin=BEGIN&end=END&fromReset=value"
 #addressTest = '<total><item total="34" direction="IN" id="0" /><item total="117" direction="OUT" id="0" /></total>'
 
-while(True):
-    markup = BeautifulSoup(open(address), "xml")
-    #markup = BeautifulSoup(addressTest, "xml")
+def main():
+    while(True):
+        markup = BeautifulSoup(open(address), "xml")
+        #markup = BeautifulSoup(addressTest, "xml")
+        
+        items = markup.find_all('item')
+        
+        for item in items:
+            if item['direction'] == "IN":
+                inCount = item['total']
+                print "IN %s" %(inCount)
+            else:
+                outCount = item['total']
+                print "OUT %s" %(outCount)
+        
+        peopleIN = inCount - outCount
+        
+        DB.updateRestroomPeopleCount(6, peopleIN)
+        
+        time.sleep(5)
 
-    items = markup.find_all('item')
-
-    for item in items:
-        if item['direction'] == "IN":
-            inCount = item['total']
-            print "IN %s" %(inCount)
-        else:
-            outCount = item['total']
-            print "OUT %s" %(outCount)
-    
-    peopleIN = inCount - outCount
-    
-    DB.updateRestroomPeopleCount(ID, peopleIN)
-    
-    time.sleep(5)
+if __name__ == '__main__':
+    main()
